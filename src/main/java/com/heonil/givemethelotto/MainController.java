d0462bfd177198141202a235359993c7d2830b23
KIM HEONIL <iamheonil@gmail.com>
2022-06-12 04:30:11 +0900

    흑흑 살려주세요

--- src/main/java/com/heonil/givemethelotto/MainController.java
+++ src/main/java/com/heonil/givemethelotto/MainController.java
@@ -3,29 +3,29 @@ package com.heonil.givemethelotto;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("")
 public class MainController {
 
     private static final Logger LOGGER = LogManager.getLogger(MainController.class);
 
     @GetMapping("/")
-    public String index(Model model) {
+    public ModelAndView index() {
 
-        // 시스아웃은 이제 그만!
+        // 시스아웃은 이제 그만
         // System.out.print("MainController");
 
         LOGGER.info("콘솔 로그 확인");
         LOGGER.debug("콘솔 로그 확인");
         LOGGER.error("콘솔 로그 확인");
 
-        model.addAttribute("hello", "MainController 입니다옹");
+        ModelAndView mav = new ModelAndView();
 
-        return "index";
+        mav.setViewName("view/main");
+
+        return mav;
     }
 
 }
