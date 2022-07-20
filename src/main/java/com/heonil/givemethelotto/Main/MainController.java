9451fc52ed45347955428e7206400789e1951a39
KIM HEONIL <iamheonil@gmail.com>
2022-07-21 00:03:47 +0900

    타임리프 오류 발생하던거 수정 ㅠ_ㅠ

--- src/main/java/com/heonil/givemethelotto/Main/MainController.java
+++ src/main/java/com/heonil/givemethelotto/Main/MainController.java
@@ -3,7 +3,8 @@ package com.heonil.givemethelotto.Main;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,7 +12,7 @@ public class MainController {
 
     private static final Logger LOGGER = LogManager.getLogger(MainController.class);
 
-    @GetMapping("/main")
+    @RequestMapping(value="/main", method = RequestMethod.GET)
     public ModelAndView index() {
 
         // 시스아웃은 이제 그만
