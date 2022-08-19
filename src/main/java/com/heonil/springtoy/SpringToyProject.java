8d70ede32e576165a746e136a0a0fac90d8033e6
KIM HEONIL <iamheonil@gmail.com>
2022-08-19 10:55:24 +0900

    프로젝트 이름 수정 중

--- src/main/java/com/heonil/springtoy/SpringToyProject.java
+++ src/main/java/com/heonil/springtoy/SpringToyProject.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GiveMeTheLottoApplication {
+public class SpringToyProject {
 
     public static void main(String[] args) {
-        SpringApplication.run(GiveMeTheLottoApplication.class, args);
+        SpringApplication.run(SpringToyProject.class, args);
     }
 
 }
